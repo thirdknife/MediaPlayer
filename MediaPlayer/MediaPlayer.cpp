e9a7e47e7b7df1463a9c116d1fca2949dc5298f1
Shakeel Shafiq <shakeel.shafique@gmail.com>
2022-05-26 02:25:14 +0000

    Add find for iterator

--- MediaPlayer/MediaPlayer.cpp
+++ MediaPlayer/MediaPlayer.cpp
@@ -16,19 +16,31 @@ int main()
 		"some",
 		"another"
 	};
-	std::list<Song> l = { 
+
+	int Id;
+
+	std:cout << "Enter id to search";
+	std::cout << '\n';
+	std::cin >> Id;
+
+	std::list<Song> songs = { 
 		{1, "some1", "another1"},
 		{2, "some2", "another2"},
 		{3, "some3", "another3"},
 		{4, "some4", "another4"},
 	};
 
-	std::for_each(l.begin(), l.end(), [](const Song n) { 
-		cout << n.id << endl;
-		cout << n.title << endl;
-		cout << n.album << endl;
-		std::cout << '\n';
-	});
+	auto n = std::find(songs.begin(), songs.end(), Id);
+	cout << n->id;
+	cout << n->title;
+	cout << n->album;
+
+	// std::for_each(songs.begin(), songs.end(), [](const Song n) { 
+	// 	cout << n.id << endl;
+	// 	cout << n.title << endl;
+	// 	cout << n.album << endl;
+	// 	std::cout << '\n';
+	// });
 
 
 	return 0;
