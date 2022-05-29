9970f9c5db251fc3f66e83ea32c8efeb4df4da75
Shakeel Shafiq <shakeel.shafique@gmail.com>
2022-05-29 14:34:16 +0000

    Use find_if

--- MediaPlayer/MediaPlayer.cpp
+++ MediaPlayer/MediaPlayer.cpp
@@ -17,11 +17,7 @@ int main()
 		"another"
 	};
 
-	int Id;
-
-	std:cout << "Enter id to search";
-	std::cout << '\n';
-	std::cin >> Id;
+	Song toSearch = {1, "some1", "another1"};
 
 	std::list<Song> songs = { 
 		{1, "some1", "another1"},
@@ -30,17 +26,17 @@ int main()
 		{4, "some4", "another4"},
 	};
 
-	auto n = std::find(songs.begin(), songs.end(), Id);
-	cout << n->id;
-	cout << n->title;
-	cout << n->album;
+	std::list<Song>::iterator it;
 
-	// std::for_each(songs.begin(), songs.end(), [](const Song n) { 
-	// 	cout << n.id << endl;
-	// 	cout << n.title << endl;
-	// 	cout << n.album << endl;
-	// 	std::cout << '\n';
-	// });
+	auto t = std::find_if( songs.begin(), songs.end(), [toSearch]( const Song& s ){ return s.id == toSearch.id; } );
+
+
+	std::for_each(songs.begin(), songs.end(), [](const Song n) { 
+		cout << n.id << endl;
+		cout << n.title << endl;
+		cout << n.album << endl;
+		std::cout << '\n';
+	});
 
 
 	return 0;
